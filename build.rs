aa7ecd1cff894913f9b02c6c96b1ccd150f8fbba
SpiralP <spiral.palmer@gmail.com>
2019-01-18 03:19:22 -0800

    setup build.rs so that we don't write bindings by default

--- build.rs
+++ build.rs
@@ -1,34 +1,38 @@
-use bindgen;
-
-#[cfg(target_os = "linux")]
-mod vars {
+#[cfg(feature = "bindgen")]
+mod builder {
+  #[cfg(target_os = "linux")]
   pub const OS: &str = "linux";
+  #[cfg(target_os = "linux")]
   pub const HEADER_PATH: &str = "/usr/include/pcap.h";
-}
 
-#[cfg(target_os = "windows")]
-mod vars {
+  #[cfg(target_os = "windows")]
   pub const OS: &str = "windows";
+  #[cfg(target_os = "windows")]
   pub const HEADER_PATH: &str = "./npcap-sdk-1.01/Include/pcap.h";
+
+  pub fn build_bindings() {
+    use bindgen;
+
+    let bindings = bindgen::builder()
+      .raw_line("#![allow(non_snake_case)]")
+      .raw_line("#![allow(non_camel_case_types)]")
+      .raw_line("#![allow(non_upper_case_globals)]")
+      .whitelist_function("pcap.*")
+      .whitelist_function("PCAP.*")
+      .header(HEADER_PATH);
+
+    #[cfg(target_os = "windows")]
+    let bindings = bindings.clang_arg("-I./npcap-sdk-1.01/Include");
+
+    let bindings = bindings.generate().unwrap();
+
+    bindings
+      .write_to_file(&format!("./src/os/{}.rs", OS))
+      .unwrap();
+  }
 }
 
 fn main() {
-  use self::vars::*;
-
-  let bindings = bindgen::builder()
-    .raw_line("#![allow(non_snake_case)]")
-    .raw_line("#![allow(non_camel_case_types)]")
-    .raw_line("#![allow(non_upper_case_globals)]")
-    .whitelist_function("pcap.*")
-    .whitelist_function("PCAP.*")
-    .header(HEADER_PATH);
-
-  #[cfg(target_os = "windows")]
-  let bindings = bindings.clang_arg("-I./npcap-sdk-1.01/Include");
-
-  let bindings = bindings.generate().unwrap();
-
-  bindings
-    .write_to_file(&format!("./src/os/{}.rs", OS))
-    .unwrap();
+  #[cfg(feature = "bindgen")]
+  self::builder::build_bindings();
 }

f05a80f7a3a96709de8f89c2fd23586be38c7776
SpiralP <spiral.palmer@gmail.com>
2019-09-07 15:08:40 -0700

    update windows sdk path version

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ mod builder {
   #[cfg(target_os = "windows")]
   pub const OS: &str = "windows";
   #[cfg(target_os = "windows")]
-  pub const HEADER_PATH: &str = "./npcap-sdk-1.01/Include/pcap.h";
+  pub const HEADER_PATH: &str = "./npcap-sdk-1.03/Include/pcap.h";
 
   #[cfg(target_os = "macos")]
   pub const OS: &str = "macos";
@@ -26,7 +26,7 @@ mod builder {
       .header(HEADER_PATH);
 
     #[cfg(target_os = "windows")]
-    let bindings = bindings.clang_arg("-I./npcap-sdk-1.01/Include");
+    let bindings = bindings.clang_arg("-I./npcap-sdk-1.03/Include");
 
     #[cfg(target_os = "macos")]
     let bindings = bindings.clang_arg("-I/usr/local/opt/libpcap/include");
@@ -45,7 +45,7 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", libdir);
   } else {
     #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-search=native=./npcap-sdk-1.01/Lib/x64");
+    println!("cargo:rustc-link-search=native=./npcap-sdk-1.03/Lib/x64");
 
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-search=native=/usr/local/opt/libpcap/lib");
